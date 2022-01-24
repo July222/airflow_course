dc7274d2f65878267ea04bd0fccb877d51132bc4
krylova-yulia <krylova-yulia@yandex-team.ru>
2022-01-24 14:23:14 +0300

    Added taskflow API DAG

--- dags/taskflow_api_dag.py
+++ dags/taskflow_api_dag.py
@@ -1,6 +1,4 @@
-from airflow import DAG
-from airflow.models import Variable
-from airflow.operators.python import PythonOperator
+from airflow.decorators import task, dag
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime, timedelta
 
@@ -10,14 +8,21 @@ class CustomPostgresOperator(PostgresOperator):
 
     template_fields = ('sql', 'parameters')
 
+@task.python
+def stage1():
+    partner_name = 'netflix'
+    partner_path = '/partners/netflix'
+    # ti.xcom_push(key="partner_name", value=partner_name)
+    return {'partner_name': partner_name, 'partner_path': partner_path}
 
-def _extract(responsible_name):
-    print(responsible_name)
+
+@task.python
+def stage2(partner_settings):
+    # partner_name = ti.xcom_pull(key="partner_name", task_ids='stage1')
+    print(partner_settings['partner_name'])
 
 
-
-with DAG(
-        dag_id="my_dag2",  # DAG id which must be unique
+@dag(
         description="this is test DAG",
         start_date=datetime(2022, 1, 5),   # the date when DAG starts being scheduled. Can be defined for each task separately. With catchup=True backfill process for old dates can start
         schedule_interval="@daily",       # interval of time fromthe min (start_date) at which DAG is triggered. We can define it by cron expression or timedelta
@@ -26,23 +31,12 @@ with DAG(
         tags=["test"],      # tags help filter DAGs
         catchup=False,       # do we need to do rerun/backfill automatically? Best practise is to set it to False
         max_active_runs=5   # how many DAG runs can be executed at the same time
-) as dag:
+)
+def my_dag_taskflow_api():      # the name of the function is dag_id
 
-    extract = PythonOperator(
-        task_id="extract",
-        python_callable=_extract,
-        # op_args=[Variable.get('my_dag_responsible', deserialize_json=True)['name']]    # deserialize_json=True is needed to parse a JSON variable
-        op_args=["{{var.json.my_dag_responsible.name}}"] # unlikely the commented line, we get the variable value only when DAG runs (not in the parse time), so we don't create additional useless DB connections
-    )
-
-    fetching_data = CustomPostgresOperator(
-        task_id="fetching_data",
-        sql="sql/MY_REQUEST.sql",
-        parameters={
-            "next_ds": '{{next_ds}}',
-            'prev_ds': '{{prev_ds}}',
-            'responsible_name': '{{var.json.my_dag_responsible.name}}'    #  these variables will be fetched during run time, not parse time
-        }
-    )
+    stage2(stage1())   # dependencies between tasks will be generated automatically
 
 
+my_dag_taskflow_api()
+
+dag = my_dag_taskflow_api()
