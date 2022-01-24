b9dc6170bf8af64a383f203fd88bb08edd96b6e3
krylova-yulia <krylova-yulia@yandex-team.ru>
2022-01-24 14:53:50 +0300

    Added taskflow API DAG

--- dags/taskflow_api_dag.py
+++ dags/taskflow_api_dag.py
@@ -1,6 +1,7 @@
 from airflow.decorators import task, dag
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime, timedelta
+from typing import Dict
 
 
 # we create a custom operator in order to make some parameters templated
@@ -22,6 +23,12 @@ def stage2(partner_settings):
     print(partner_settings['partner_name'])
 
 
+@task.python
+def stage3() -> Dict[str, str]:     # in this case each dict value becomes a separate xcom value
+    partner_name = 'amazon'
+    partner_path = '/partners/amazon'
+    return {'partner_name': partner_name, 'partner_path': partner_path}
+
 @dag(
         description="this is test DAG",
         start_date=datetime(2022, 1, 5),   # the date when DAG starts being scheduled. Can be defined for each task separately. With catchup=True backfill process for old dates can start
@@ -35,8 +42,7 @@ def stage2(partner_settings):
 def my_dag_taskflow_api():      # the name of the function is dag_id
 
     stage2(stage1())   # dependencies between tasks will be generated automatically
+    stage3()
 
-
-my_dag_taskflow_api()
 
 dag = my_dag_taskflow_api()
